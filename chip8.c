01bacdfa930fe376555221beae29537eeaa84d6c
Jason Chu <jasonchu177@gmail.com>
2025-10-29 23:25:41 -0400

    completed the quit function for the screen

--- chip8.c
+++ chip8.c
@@ -3,21 +3,32 @@
 #include <stdbool.h>
 #include <SDL.h>
 #include "SDL.h"
-
+// Container
 typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
 } sdl_t;
+//configuration object
 typedef struct {
     uint32_t window_width; //SDL window width
     uint32_t window_height; //SDL window height
     uint32_t fg_color; //Foreground color
     uint32_t bg_color; //Background color
     uint32_t scale_factor; // Amount to scale a chip8 pixel by e.g. 20x will be 20x larger window
-
-
-
 }config_t;
+//Emulator states
+typedef enum {
+    QUIT  ,
+    RUNNING ,
+    PAUSED ,
+}emulatior_state_t;
+
+//CHIP8 Machine object
+typedef struct{
+    emulatior_state_t state;
+}chip8_t;
+
+
 
 //initialize SDL
 bool init_sdl(sdl_t *sdl, const config_t config) {
@@ -64,6 +75,11 @@ bool set_config_from_args(config_t *config, int argc, char **argv){
 
     return true;
 }
+// initialize chip8 machine
+bool init_chip8(chip8_t *chip8){
+    chip8->state = RUNNING; //default on
+    return true; //success
+}
 
 //final Cleanup
 void final_cleanup(const sdl_t sdl) {
@@ -89,6 +105,33 @@ void update_screen(const sdl_t sdl) {
 
 }
 
+//handle inputs
+void handle_input(chip8_t *chip8){
+    
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+            switch (event.type){
+                case SDL_QUIT:
+                    //end program
+                    chip8->state = QUIT; //exits the main loop
+                    return;
+                case SDL_KEYDOWN:
+                    break;
+
+                case SDL_KEYUP:
+                    break;
+
+                default:
+                    break;
+                //exit window
+                
+            }
+            // if (event.type == SDL_QUIT) {
+            //     running = false;
+            // }
+        }
+    // return running;
+}
 //da main
 
 int main(int argc, char **argv) {
@@ -101,21 +144,19 @@ int main(int argc, char **argv) {
     sdl_t sdl = {0};
     if (!init_sdl(&sdl, config)) exit(EXIT_FAILURE);
 
+    //initialize chip8 machine
+    chip8_t chip8 = {0};
+    if (!init_chip8(&chip8)) exit(EXIT_FAILURE);
     //initial screen clear to backroud color
     clear_screen(sdl, config);
 
     // Main emulator loop 
-    bool running = true;
-     while (running) {
-        // Handle user inputs 
 
+     while (chip8.state != QUIT) {
+        // Handle user inputs 
         // Handle events - required for macOS
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
+        handle_input(&chip8);
+        // if chip8 state paused continue;
 
         //get_time()
         //emulate chip8 instructions
